0d293e5098c3fb295d0944e5c58c0f2e1a76bf7a
Wendell Rodrigues <101348428+Wendell26@users.noreply.github.com>
2024-12-24 14:53:12 -0300

    Update login.sh

--- login/login.sh
+++ login/login.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-if [ ! -d "$HOME/.creds" ]; then
-zenity --error --text "Error: no default credentials found. You can edit it on $HOME/.creds/(name.txt, passwd.txt and user.txt)."
-pkill Xvnc
-exit 0
-fi
-MAIN_USER=$(cat $HOME/.creds/user.txt)
-MAIN_PASS=$(cat $HOME/.creds/passwd.txt)
-USER=$(zenity --entry --text "Username")
-PASS=$(zenity --password --text "Password")
+if [ ! -f "$HOME/.vnc/login/name.txt" ] && [ ! -f "$HOME/.vnc/login/user.txt" ] && [ ! -f "$HOME/.vnc/login/passwd.txt" ]; then                         zenity --error --text "Error: no default credentials found. You can edit it on $HOME/.vnc/login (name.txt, passwd.txt and user.txt)."                   pkill Xvnc                                                                                                                                              exit 0                                                                                                                                                  fi
+MAIN_USER=$(cat $HOME/.vnc/login/user.txt)                                                                                                              MAIN_PASS=$(cat $HOME/.vnc/login/passwd.txt)                                                                                                            USER=$(zenity --entry --text "Username")                                                                                                                PASS=$(zenity --password --text "Password")
 if [ "$MAIN_USER" == "$USER" ] && [ "$MAIN_PASS" == "$PASS" ]; then
-xmessage -center "Welcome, $(cat $HOME/.creds/name.txt)!"
+zenity --info --text "Logged sucessfully. Welcome, $(cat $HOME/.vnc/login/name.txt)!"
 else
 zenity --error --text "Invalid credentials. Try again later."
 pkill Xvnc
 exit 0
 fi
-
